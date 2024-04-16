8532c23317895e34c7b83a7d118b7c5545d27de1
Pjumo <parkjunmoa@naver.com>
2024-04-16 19:44:04 +0900

    [Style] Graph 코드 변경

--- make_graph.py
+++ make_graph.py
@@ -1,31 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-model_name = 'resnet18'
-train_loss = np.load(f'result_numpy/{model_name}/train_loss.npy')
-train_acc = np.load(f'result_numpy/{model_name}/train_acc.npy')
-val_loss = np.load(f'result_numpy/{model_name}/val_loss.npy')
-val_acc = np.load(f'result_numpy/{model_name}/val_acc.npy')
+graph_name = 'ResNet vs Wide ResNet'
+
+# model_name = 'resnet18'
+# train_acc = np.load(f'result_numpy/{model_name}/train_acc.npy')
+#
+# model2_name = 'resnet34'
+# train2_acc = np.load(f'result_numpy/{model2_name}/train_acc.npy')
+
+models_name = ['resnet34', 'wide_resnet34']
 
 # plot Loss Graph
-plt.subplot(2, 1, 1)
-plt.plot(train_loss)
-plt.plot(val_loss)
-plt.title('model loss')
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.ylim(0, 2)
-plt.legend(['train', 'validation'], loc='upper left')
+# plt.subplot(2, 1, 1)
+# plt.plot(train_loss)
+# plt.plot(val_loss)
+# plt.title('model loss')
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.ylim(0, 2)
+# plt.legend(['train', 'validation'], loc='upper left')
 
 # plot Acc Graph
-plt.subplot(2, 1, 2)
-plt.plot(train_acc * 100)
-plt.plot(val_acc * 100)
-plt.title('model acc')
+# plt.subplot(2, 1, 2)
+for model_name in models_name:
+    train_acc = np.load(f'result_numpy/{model_name}/train_acc.npy')
+    plt.plot(1-train_acc)
+
+# plt.plot(1-train_acc)
+# plt.plot(1-train2_acc)
+plt.yscale('log', base=10)
+plt.ylim(1e-2, 1)
+plt.title(graph_name)
 plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.ylim(0, 100)
-plt.legend(['train', 'validation'], loc='upper left')
+plt.ylabel('error rate')
+plt.legend(models_name, loc='upper right')
 
 plt.tight_layout()
-plt.savefig(f'graphs/{model_name}.png')
+plt.savefig(f'graphs/{graph_name}.png')
