58bbe5a665b114acdecd40835e324bc282d8ec7f
Pjumo <parkjunmoa@naver.com>
2024-04-17 01:21:26 +0900

    [Style] model_test.py DataLoader num_workers=4로 변경

--- model_test.py
+++ model_test.py
@@ -8,166 +8,176 @@ import torch
 import torch.nn as nn
 
 from torchvision import datasets, transforms
-
 from torch.utils.data.sampler import SubsetRandomSampler
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-num_epochs = 30
-batch_size = 64
+def fun():
+    torch.multiprocessing.freeze_support()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.cuda.empty_cache()
 
-# augment *padding *HorizontalFlip
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    # CIFAR-100 (0.5074, 0.4867, 0.4411), (0.2011, 0.1987, 0.2025)
-    # transforms.Resize(224, 224)
-])
+    num_epochs = 50
+    batch_size = 16
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    # transforms.Resize(224, 224)
-])
+    # augment *padding *HorizontalFlip
+    transform_train = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.RandomCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        # CIFAR-10 (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+        # CIFAR-100 (0.5074, 0.4867, 0.4411), (0.2011, 0.1987, 0.2025)
+        # CIFAR-100 scaled 224X224 (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+    ])
 
-# Call CIFAR datasets
-train_dataset = datasets.CIFAR10('./train', train=True, transform=transform_train, download=True)
-val_dataset = datasets.CIFAR10('./train', train=True, transform=transform_test, download=True)
-test_dataset = datasets.CIFAR10('./test', train=False, transform=transform_test, download=True)
+    transform_test = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
 
-# Split train datasets for validation datasets
-num_train = len(train_dataset)
-indices = list(range(num_train))
-split = int(np.floor(0.2 * num_train))
-train_idx, val_idx = indices[split:], indices[:split]
+    # Call CIFAR datasets
+    train_dataset = datasets.CIFAR100('./train', train=True, transform=transform_train, download=True)
+    val_dataset = datasets.CIFAR100('./train', train=True, transform=transform_test, download=True)
+    test_dataset = datasets.CIFAR100('./test', train=False, transform=transform_test, download=True)
 
-val_sampler = SubsetRandomSampler(val_idx)
-np.random.shuffle(indices)
-train_sampler = SubsetRandomSampler(train_idx)
+    # Split train datasets for validation datasets
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(0.2 * num_train))
+    train_idx, val_idx = indices[split:], indices[:split]
 
-# Create DataLoader (split by batch size)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=train_sampler)
-val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, sampler=val_sampler)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    val_sampler = SubsetRandomSampler(val_idx)
+    np.random.shuffle(indices)
+    train_sampler = SubsetRandomSampler(train_idx)
 
-cnt_progress = len(train_loader) // 30
+    # Create DataLoader (split by batch size)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=train_sampler,
+                                               num_workers=4)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, sampler=val_sampler,
+                                             num_workers=4)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-model_name = 'u2net'
-model = model_loader.load(model_name, num_class=10).to(device)
+    cnt_progress = len(train_loader) // 30
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
-loss_func = nn.CrossEntropyLoss()
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=10, gamma=0.1)
+    model_name = 'resnext50'
+    model = model_loader.load(model_name, num_class=100).to(device)
 
-best_val_loss = float('Inf')
-train_loss = []
-val_loss = []
-train_acc = []
-val_acc = []
-recent_val_loss = 100
-for epoch in range(num_epochs):
-    model.train()
-    total_train_acc = 0
-    total_train_loss = 0
-    for cnt, (images_, labels_) in enumerate(train_loader):
-        images = images_.to(device)
-        labels = labels_.to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+    loss_func = nn.CrossEntropyLoss()
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[30, 40], gamma=0.1)
 
-        predict = model(images)
-        loss = loss_func(predict, labels.long())
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        total_train_acc += (predict.argmax(1) == labels).float().mean().item()
-        total_train_loss += loss.item()
-        # progress bar 구현
-        if cnt % cnt_progress == 0:
-            print(f'\rEpoch {epoch + 1} [', end='')
-            for prog in range(cnt // cnt_progress):
-                print('■', end='')
-            for prog in range((len(train_loader) - cnt) // cnt_progress):
-                print('□', end='')
-            print(']', end='')
-
-    total_train_loss /= len(train_loader)
-    total_train_acc /= len(train_loader)
-    train_loss.append(total_train_loss)
-    train_acc.append(total_train_acc)
-    print(f' - Train Loss: {total_train_loss:.4f}, Train Acc: {total_train_acc:.4f}')
-
-    model.eval()  # 모델을 평가 모드로 설정
-    total_val_acc = 0
-    total_val_loss = 0
-    num_loss = 0
-    with torch.no_grad():
-        for images_, labels_ in val_loader:
+    best_val_loss = float('Inf')
+    train_loss = []
+    val_loss = []
+    train_acc = []
+    val_acc = []
+    for epoch in range(num_epochs):
+        model.train()
+        total_train_acc = 0
+        total_train_loss = 0
+        for cnt, (images_, labels_) in enumerate(train_loader):
             images = images_.to(device)
             labels = labels_.to(device)
 
             predict = model(images)
+            loss = loss_func(predict, labels.long())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_train_acc += (predict.argmax(1) == labels).float().mean().item()
+            total_train_loss += loss.item()
+            # progress bar 구현
+            if cnt % cnt_progress == 0:
+                print(f'\rEpoch {epoch + 1} [', end='')
+                for prog in range(cnt // cnt_progress):
+                    print('■', end='')
+                for prog in range((len(train_loader) - cnt) // cnt_progress):
+                    print('□', end='')
+                print(']', end='')
+
+        total_train_loss /= len(train_loader)
+        total_train_acc /= len(train_loader)
+        train_loss.append(total_train_loss)
+        train_acc.append(total_train_acc)
+        print(f' - Train Loss: {total_train_loss:.4f}, Train Acc: {total_train_acc:.4f}')
+
+        model.eval()  # 모델을 평가 모드로 설정
+        total_val_acc = 0
+        total_val_loss = 0
+        num_loss = 0
+        with torch.no_grad():
+            for images_, labels_ in val_loader:
+                images = images_.to(device)
+                labels = labels_.to(device)
+
+                predict = model(images)
+                acc = (predict.argmax(1) == labels).float().mean().item()
+                loss = loss_func(predict, labels.long())
+                total_val_acc += acc
+                total_val_loss += loss.item()
+
+            total_val_loss /= len(val_loader)
+            total_val_acc /= len(val_loader)
+
+        val_loss.append(total_val_loss)
+        val_acc.append(total_val_acc)
+        print(f'Validation Loss: {total_val_loss:.4f}, Validation Acc: {total_val_acc:.4f}')
+
+        if total_val_loss < best_val_loss:
+            num_loss = 0
+            best_val_loss = total_val_loss
+            torch.save(model.state_dict(), 'best_model.pt')
+        # else:  # early stopping
+        #     num_loss += 1
+        #     if num_loss == 5:
+        #         print(f'early stopping: epoch {epoch}')
+        #         break
+
+        scheduler.step()
+
+    model.load_state_dict(torch.load('best_model.pt'))
+    model.eval()
+
+    total_test_acc = 0
+    total_test_loss = 0
+    with torch.no_grad():
+        for images_, labels_ in test_loader:
+            images = images_.to(device)
+            labels = labels_.to(device)
+
+            predict = model(images)
+
             acc = (predict.argmax(1) == labels).float().mean().item()
             loss = loss_func(predict, labels.long())
-            total_val_acc += acc
-            total_val_loss += loss.item()
 
-        total_val_loss /= len(val_loader)
-        total_val_acc /= len(val_loader)
+            total_test_acc += acc
+            total_test_loss += loss.item()
 
-    val_loss.append(total_val_loss)
-    val_acc.append(total_val_acc)
-    print(f'Validation Loss: {total_val_loss:.4f}, Validation Acc: {total_val_acc:.4f}')
+        total_test_loss /= len(test_loader)
+        total_test_acc /= len(test_loader)
 
-    if total_val_loss < best_val_loss:
-        num_loss = 0
-        best_val_loss = total_val_loss
-        torch.save(model.state_dict(), 'best_model.pt')
-    else:
-        num_loss += 1
-        if num_loss == 5:
-            print(f'early stopping: epoch {epoch}')
-            break
+    print(f'Test Loss: {total_test_loss:.4f}, Test Acc: {total_test_acc:.4f}')
 
-    scheduler.step()
+    train_loss = np.array(train_loss)  # 각 모델의 loss, acc 정보를 저장 (추후 그래프를 위함)
+    train_acc = np.array(train_acc)
+    val_loss = np.array(val_loss)
+    val_acc = np.array(val_acc)
+    test_result = np.array([total_test_loss, total_test_acc, num_epochs])
 
-model.load_state_dict(torch.load('best_model.pt'))
-model.eval()
+    os.makedirs(f'result_numpy/{model_name}', exist_ok=True)
+    np.save(os.path.join(f'result_numpy/{model_name}/train_loss'), train_loss)
+    np.save(os.path.join(f'result_numpy/{model_name}/train_acc'), train_acc)
+    np.save(os.path.join(f'result_numpy/{model_name}/val_loss'), val_loss)
+    np.save(os.path.join(f'result_numpy/{model_name}/val_acc'), val_acc)
+    np.save(os.path.join(f'result_numpy/{model_name}/test_result'), test_result)
 
-total_test_acc = 0
-total_test_loss = 0
-with torch.no_grad():
-    for images_, labels_ in test_loader:
-        images = images_.to(device)
-        labels = labels_.to(device)
+    model = model.to("cpu")
+    torch.cuda.empty_cache()
 
-        predict = model(images)
 
-        acc = (predict.argmax(1) == labels).float().mean().item()
-        loss = loss_func(predict, labels.long())
-
-        total_test_acc += acc
-        total_test_loss += loss.item()
-
-    total_test_loss /= len(test_loader)
-    total_test_acc /= len(test_loader)
-
-print(f'Test Loss: {total_test_loss:.4f}, Test Acc: {total_test_acc:.4f}')
-
-train_loss = np.array(train_loss)  # 각 모델의 loss, acc 정보를 저장 (추후 그래프를 위함)
-train_acc = np.array(train_acc)
-val_loss = np.array(val_loss)
-val_acc = np.array(val_acc)
-test_result = np.array([total_test_loss, total_test_acc, num_epochs])
-
-os.makedirs(f'result_numpy/{model_name}', exist_ok=True)
-np.save(os.path.join(f'result_numpy/{model_name}/train_loss'), train_loss)
-np.save(os.path.join(f'result_numpy/{model_name}/train_acc'), train_acc)
-np.save(os.path.join(f'result_numpy/{model_name}/val_loss'), val_loss)
-np.save(os.path.join(f'result_numpy/{model_name}/val_acc'), val_acc)
-np.save(os.path.join(f'result_numpy/{model_name}/test_result'), test_result)
-
-model = model.to("cpu")
-torch.cuda.empty_cache()
+if __name__ == '__main__':
+    fun()
--- models/resnext.py
+++ models/resnext.py
@@ -13,28 +13,29 @@ class ResNextBottleNeck(nn.Module):
 
         num_depth = int(self.expansion * out_channels / 64)  # 그룹당 채널 수(depth per group)
         self.split_block = nn.Sequential(
-            nn.BatchNorm2d(in_channels),
-            nn.ReLU(),
-            nn.Conv2d(in_channels, groups * num_depth, kernel_size=1, groups=groups, bias=False),
+            nn.Conv2d(in_channels, groups * num_depth, kernel_size=1, stride=1, groups=groups, bias=False),
             nn.BatchNorm2d(groups * num_depth),
             nn.ReLU(),
             nn.Conv2d(groups * num_depth, groups * num_depth, kernel_size=3, stride=stride, groups=groups, padding=1,
                       bias=False),
             nn.BatchNorm2d(groups * num_depth),
             nn.ReLU(),
-            nn.Conv2d(groups * num_depth, out_channels * 4, kernel_size=1, bias=False)
+            nn.Conv2d(groups * num_depth, out_channels * self.expansion, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels * self.expansion),
+            nn.ReLU(),
         )
 
-        if self.stride != 1 or self.in_channels != self.out_channels * 4:
+        if self.stride != 1 or self.in_channels != self.out_channels * self.expansion:
             self.down_sample = nn.Sequential(
-                nn.BatchNorm2d(self.in_channels),
-                nn.ReLU(),
-                nn.Conv2d(self.in_channels, self.out_channels * 4, kernel_size=1, stride=stride, bias=False)
+                nn.Conv2d(self.in_channels, self.out_channels * self.expansion, kernel_size=1, stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(self.out_channels * self.expansion),
+                nn.ReLU()
             )
 
     def forward(self, x):
         out = self.split_block(x)
-        if self.stride != 1 or self.in_channels != self.out_channels * 4:
+        if self.stride != 1 or self.in_channels != self.out_channels * self.expansion:
             x = self.down_sample(x)
         out = x + out
         return out
@@ -47,17 +48,17 @@ class ResNext(nn.Module):
         self.in_channels = 64
 
         self.base = nn.Sequential(
-            nn.Conv2d(3, self.in_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(self.in_channels),
             nn.ReLU()
         )
-
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self.make_layer(64, num_blocks[0], stride=1)
         self.layer2 = self.make_layer(128, num_blocks[1], stride=2)
         self.layer3 = self.make_layer(256, num_blocks[2], stride=2)
         self.layer4 = self.make_layer(512, num_blocks[3], stride=2)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.avgpool = nn.AvgPool2d(7)
         self.fc = nn.Linear(512 * block_.expansion, num_classes)
 
     def make_layer(self, out_channels, num_block, stride):
@@ -72,6 +73,7 @@ class ResNext(nn.Module):
 
     def forward(self, x):
         out = self.base(x)
+        out = self.maxpool(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
